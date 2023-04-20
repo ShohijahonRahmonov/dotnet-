a1d623f407dd92e13d58dfd25a6ab61d4a10a472
Shohijahon Rahmonov <shohijahonrahmonov2023@gmail.com>
2023-04-20 22:10:52 +0500

    dotnet

---
+++ oop/c.cs
@@ -0,0 +1,35 @@
+    // Mavzu : Class va obectlar 
+ 
+ using System; 
+// namespace oop.name
+// {
+//     class Salom 
+//     {
+//         static void Main (string [] args)
+//         {
+//             inson odam = new inson ();
+//             Console.WriteLine ($"Bu insoning ismi {odam.Name}");
+//             Console.WriteLine ($"Bu insoning familiyasi {odam.surName}");
+//             Console.WriteLine($"Bu insoning yoshi {odam.age}");
+//             Console.WriteLine ($"Bu insoning kasbi {odam.job}");
+//             Console.ReadKey();
+
+//         }
+//     }
+// }
+
+// namespace oop.hayvon
+// {
+//     class h
+//     {
+//         static void Main (string [] args)
+//         {
+//             animal arslon =  new animal();
+//             Console.WriteLine(arslon.name);
+//             Console.WriteLine (arslon.color);
+//             Console.WriteLine (arslon.age);
+//             Console.WriteLine (arslon.food);
+//         }
+//     }
+// }
+
---
+++ oop/class.cs
@@ -0,0 +1,24 @@
+  // Mavzu: Class va obectlar
+using System;
+// namespace oop.name
+// {
+//     public class inson
+//     {
+//         public string Name = "Shohijahon";
+//         public string surName  = "Rahmonov";
+//         public string age  = "20";
+//         public string job = "student";
+
+ // }
+//}
+
+// namespace oop.hayvon
+// {
+//    public  class animal
+//     {
+//         public string name = "Arslon";
+//         public string color = "brown";
+//         public int age = 4;
+//         public string food = "Meat";
+//     }
+// }
