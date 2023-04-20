d9e0f8624403fc45e37062eca08922c273c556b6
Shohijahon Rahmonov <shohijahonrahmonov2023@gmail.com>
2023-04-20 22:04:17 +0500

    classlar bilan ishlash

--- turi.cs
+++
@@ -1,9 +0,0 @@
-﻿Console.Write("Ismingizni kiriting ");
-string name  = Console.ReadLine();
-Console.WriteLine($"Salom {name}");
-Console.Write("Yoshingizni kiriting ");
-string yosh = Console.ReadLine();
-Console.WriteLine("Yuklanmoqda.......");
-
-int age = Convert.ToInt32(yosh);
-Console.WriteLine($"{name} ismli foydalanuvchi {yosh} yoshda ekan");
